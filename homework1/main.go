9fa9e242ee596c8acd06ceb353c90c81cbf3b9d5
Krasul1a <dimontus02140@gmail.com>
2022-06-14 19:34:09 +0300

    number: 42

--- homework1/main.go
+++ homework1/main.go
@@ -25,18 +25,24 @@ func main() {
 	var Wallet float32 = 23
 	var ShoppingCart float32 = 0
 
-	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?")
+	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?")
 	ShoppingCart = Apple*9 + Pear*8
 	fmt.Println(ShoppingCart, "грн")
 
-	fmt.Println("2. Скільки груш ми можемо купити?")
+	fmt.Println("Скільки груш ми можемо купити?")
 	ShoppingCart = float32(int(Wallet / Pear))
-	fmt.Println(ShoppingCart)
+	fmt.Println(ShoppingCart, "штук")
 
-	fmt.Println("3. Скільки яблук ми можемо купити?")
+	fmt.Println("Скільки яблук ми можемо купити?")
 	ShoppingCart = float32(int(Wallet / Apple))
-	fmt.Println(ShoppingCart)
+	fmt.Println(ShoppingCart, "штук")
 
-	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?")
+	fmt.Println("Чи ми можемо купити 2 груші та 2 яблука?")
+	ShoppingCart = Apple*2 + Pear*2
+	if Wallet >= ShoppingCart {
+		fmt.Println("Так!")
+	} else {
+		fmt.Println("Ні!")
+	}
 
 }
